fe8f6f6bc7769716efe16dae8ac4efa8fbc2a428
estebanfelipep <estebanfelipe127@gmail.com>
2025-10-05 11:49:38 -0500

    Finish exercises 01.3 and 01.4

--- exercises/01-basics/01.3-generating-text/problem/main.ts
+++ exercises/01-basics/01.3-generating-text/problem/main.ts
@@ -3,10 +3,10 @@ import { generateText } from 'ai';
 
 // TODO: Choose a model. I recommend using the Google Gemini model:
 // gemini-2.0-flash-lite
-const model = TODO;
+const model = google('gemini-2.0-flash-lite');
 
-const prompt = 'What is the capital of France?';
+const prompt = 'What is the capital of Colombia?';
 
-const result = TODO; // TODO: Use generateText to get the result
+const result = await generateText({ prompt, model }); // TODO: Use generateText to get the result
 
 console.log(result.text);
